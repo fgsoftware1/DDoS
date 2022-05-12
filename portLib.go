3a3d95881d4b64831ef81134465ae0c44a02f46a
fgygh <fgygh@fboi.ga>
2022-05-12 09:34:16 +0100

    commit
    Signed-off-by: fgygh <fgygh@fboi.ga>

---
+++ portLib.go
@@ -0,0 +1,16 @@
+package portLib
+
+import (
+	"net"
+)
+
+//free TCP port available
+func Get() (port int, err error) {
+	ln, err := net.Listen("tcp", "[::]:0")
+	if err != nil {
+		return 0, err
+	}
+	port = ln.Addr().(*net.TCPAddr).Port
+	err = ln.Close()
+	return
+}
